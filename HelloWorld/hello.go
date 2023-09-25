0bc385ad7ce6e0c77b2572fd84a16aafc13ed828
safwank97 <safwank97@gmail.com>
2023-09-25 10:35:56 -0400

    Phase 1 of refactored of the code to pass a failing test condition with empty string

--- HelloWorld/hello.go
+++ HelloWorld/hello.go
@@ -4,15 +4,29 @@ import "fmt"
 
 /*
 func Hello() string {
-	return "Hello, world !!!"
+	return "Hello, World !!!"	//Replace World with your name to understand difference better.
 }
 */
 
+/*
 func HelloGreetings(name string) string {
 	return "Hello, " + name
 }
+*/
+
+const englishHelloPrefix = "Hello, "
+
+func HelloWithConstatnts(name string) string {
+
+	if name == "" {
+		name = "World !!!"
+	}
+
+	return englishHelloPrefix + name
+}
 
 func main() {
-	fmt.Println(HelloGreetings("Safwan !!!"))
 	//fmt.Println(Hello())
+	//fmt.Println(HelloGreetings("World !!!"))
+	fmt.Println(HelloWithConstatnts("World !!!")) //Replace World with your name to understand difference better.
 }
--- HelloWorld/hello_test.go
+++ HelloWorld/hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 /*func TestHello(t *testing.T) {
 	got := Hello()
-	want := "Hello, world !!!"
+	want := "Hello, World !!!"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
@@ -12,11 +12,37 @@ import "testing"
 }
 */
 
+/*
 func TestHelloGreetings(t *testing.T) {
+
+	//Purposeful failing test with a name and not the string world
 	got := HelloGreetings("Safwan !!!")
 	want := "Hello, Safwan !!!"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+}
+*/
+
+func TestHelloWithConstants(t *testing.T) {
+
+	//Purposeful failing test with empty string and handling that,
+	//while making changes to the .go file by passing a string under if string is empty then what to do
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := HelloWithConstatnts("Safwan !!!")
+		want := "Hello, Safwan !!!"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("say 'Hello World !!!' when an empty string is supplied ", func(t *testing.T) {
+		got := HelloWithConstatnts("")
+		want := "Hello, World !!!"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
